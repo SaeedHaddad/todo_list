4ebd46afa3bb5975d3931de2ddcb5eb6ace81cd0
Saeed Haddad <haddadsaeed6@gmail.com>
2024-12-19 09:32:41 +0300

    .

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -13,14 +13,14 @@ function Home() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3001/get")
+      .get("http://0.0.0.0:3001/get")
       .then((result) => setTodos(result.data))
       .catch((err) => console.log(err));
   }, []);
 
   const handleEdit = (id) => {
     axios
-      .put(`http://localhost:3001/update/${id}`)
+      .put(`http://0.0.0.0:3001/update/${id}`)
       .then((result) => {
         location.reload();
       })
@@ -29,7 +29,7 @@ function Home() {
 
   const handleDelete = (id) => {
     axios
-      .delete(`http://localhost:3001/delete/${id}`)
+      .delete(`http://0.0.0.0:3001/delete/${id}`)
       .then((result) => {
         location.reload();
       })
