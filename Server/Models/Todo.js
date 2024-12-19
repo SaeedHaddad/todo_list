ce4da2f2783b7483c5aff18be54c74c53c33d707
Saeed Haddad <haddadsaeed6@gmail.com>
2024-12-19 10:27:39 +0300

    .

--- Server/Models/Todo.js
+++ Server/Models/Todo.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema({
-  task: String,
+  task: { type: String, required: true },
   done: { type: Boolean, default: false },
 });
 
-const TodoModel = mongoose.model("todos", TodoSchema);
+const TodoModel = mongoose.model("Todo", TodoSchema);
 module.exports = TodoModel;
